21b3c61cf7563937eaae5566bb532cac0621c912
Camilo Mercado <j.mercado@uniandes.edu.co>
2024-01-19 14:30:46 -0500

    typos and routing corrected

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'navbar-component',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+
+
+  constructor(private router: Router) { }
+  
+  navigateTo(route: string) {
+    this.router.navigate([`${route}`]);
+  }
 
 }
