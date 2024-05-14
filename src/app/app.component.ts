566a8ba392b2ca7a2eb081fd28122c0bafb41fb2
Camilo Mercado <j.mercado@uniandes.edu.co>
2024-05-13 23:05:36 -0500

    href updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,14 @@ import * as AOS from 'aos';
 export class AppComponent implements OnInit {
 
   ngOnInit() {
+    (function(d, m){
+        var kommunicateSettings = 
+            {"appId":"2a0fee240dfca78675265d3628d942ecc","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        (window as any).kommunicate = m; m._globals = kommunicateSettings;
+    })(document, (window as any).kommunicate || {});
     AOS.init();
     window.addEventListener('load', AOS.refresh);
   }
--- src/app/components/assistant/assistant.component.ts
+++ src/app/components/assistant/assistant.component.ts
@@ -15,14 +15,14 @@ export class AssistantComponent implements OnInit {
   }
 
   ngOnInit(): void {
-        (function(d, m){
-        var kommunicateSettings = 
-            {"appId":"2a0fee240dfca78675265d3628d942ecc","popupWidget":true,"automaticChatOpenOnNavigation":true};
-        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
-        (window as any).kommunicate = m; m._globals = kommunicateSettings;
-    })(document, (window as any).kommunicate || {});
+    //     (function(d, m){
+    //     var kommunicateSettings = 
+    //         {"appId":"2a0fee240dfca78675265d3628d942ecc","popupWidget":true,"automaticChatOpenOnNavigation":true};
+    //     var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+    //     s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+    //     var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+    //     (window as any).kommunicate = m; m._globals = kommunicateSettings;
+    // })(document, (window as any).kommunicate || {});
   }
 
   navigateTo(route: string, number?: number) {
